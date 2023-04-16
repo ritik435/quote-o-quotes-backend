bab68545e5fa2d5f72b4789bf63c38d660c19ad6
iamshivambatra <mihirbatra65@gmail.com>
2023-04-16 11:16:51 +0530

    added new api

--- index.js
+++ index.js
@@ -6,6 +6,9 @@ const app = Express();
 app.get('/',(req,res)=>{
     res.send('<h1>Its the backend</h1>');
 })
+app.get('/about',(req,res)=>{
+    res.send('<h1>Its the about</h1>');
+})
 app.listen(PORT,()=>{
     console.log(`App is listening to http://localhost:${PORT}`);
 })
